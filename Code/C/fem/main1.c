b5f0e1ee3555e0f852bb17f1073304db2eb6ca61
fmckenna <fmckenna@berkeley.edu>
2019-07-23 01:29:33 -0700

    fmk - fixing main args and setting default values

--- Code/C/fem/main1.c
+++ Code/C/fem/main1.c
@@ -4,6 +4,8 @@
 int main(int argc, char **argv) {
   Domain theDomain;
   theDomain.theNodes=0;
+  theDomain.numNodes=0;
+  theDomain.maxNumNodes=0;
   domainAddNode(&theDomain, 1, 0.0, 0.0);
   domainAddNode(&theDomain, 2, 0.0, 2.0);
   domainAddNode(&theDomain, 3, 1.0, 1.0);
